13cf52ffe158aec87ca8e57c3f772a8fca50de6c
kpastula <kacperpastula4@gmail.com>
2025-11-28 21:01:01 +0100

    zadanie 3

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import numpy.polynomial.polynomial as nppoly
 
 
 def roots_20(coef: np.ndarray) -> tuple[np.ndarray, np.ndarray] | None:
+    
     coef = coef + np.random.random_sample(coef.shape) * 1e-10
     roots = nppoly.polyroots(coef)
     return coef,roots
@@ -34,12 +35,12 @@ def roots_20(coef: np.ndarray) -> tuple[np.ndarray, np.ndarray] | None:
 def frob_a(coef: np.ndarray) -> np.ndarray | None:
     if not isinstance(coef, np.ndarray) or coef.ndim != 1 or coef[0] == 0 or len(coef) < 2:
         return None
-    coef_rev = np.delete(coef[::-1],-1)
+    coef_an = np.delete(coef,-1)
     eye = np.eye(len(coef)-2)
     zeros = np.zeros((len(coef)-2,1))
     zeros_eye = np.concatenate((zeros,eye),axis=1)
-    coef_row = coef_rev.reshape(1,-1)
-    F = np.concatenate((zeros_eye,-coef_row/coef[0]),axis=0)
+    coef_row = coef_an.reshape(1,-1)
+    F = np.concatenate((zeros_eye,-coef_row/coef[-1]),axis=0)
     return F
     """Funkcja służąca do wyznaczenia macierzy Frobeniusa na podstawie
     współczynników jej wielomianu charakterystycznego:
